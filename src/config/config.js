40b82c82b7e6de435ba6d7ff4cb0d6018d318542
Rehman141243 <abdulrehman141243@gmail.com>
2026-05-02 16:11:49 +0500

    employee-responsivenss

--- src/config/config.js
+++ src/config/config.js
@@ -7,22 +7,12 @@
 export const config = {
   apiUrl:
     environment === "production"
-      ? process.env.NEXT_PUBLIC_API_URL_PROD
+      ? process.env.NEXT_PUBLIC_API_URL
       : environment === "staging"
       ? process.env.NEXT_PUBLIC_API_URL_STAGING
       : process.env.NEXT_PUBLIC_API_URL_DEV,
 
-  identityUrl:
-    environment === "production"
-      ? process.env.NEXT_PUBLIC_IDENTITY_URL_PROD
-      : environment === "staging"
-      ? process.env.NEXT_PUBLIC_IDENTITY_URL_STAGING
-      : process.env.NEXT_PUBLIC_IDENTITY_URL_DEV,
-
-  socketUrl:
-    environment === "production" || environment === "staging"
-      ? process.env.NEXT_PUBLIC_SOCKET_URL_PROD
-      : process.env.NEXT_PUBLIC_SOCKET_URL_DEV,
+  
 };
 
 export default config;
