ec11c724cf2d037679f6f32917de0b2a5c684a89
saboor000 <saboorsamad56@gmail.com>
2026-05-04 11:57:12 +0500

    deployment

--- src/config/config.js
+++ src/config/config.js
@@ -1,32 +1,18 @@
-// // config.js
+// config.js
 
-// //let environment = "dev";
-// // let environment = "staging";
-//  let environment = "production";
+// let environment = "dev";
+// let environment = "staging";
+ let environment = "production";
 
-//  const config = {
-//   apiUrl:
-//     environment === "production"
-//       ? process.env.NEXT_PUBLIC_API_URL
-//       : environment === "staging"
-//       ? process.env.NEXT_PUBLIC_API_URL_STAGING
-//       : process.env.NEXT_PUBLIC_API_URL_DEV,
-
-  
-// };
-
-// export default config;
-
-
-const environment = "production"; // change to dev / staging / production
-
-const config = {
+ const config = {
   apiUrl:
     environment === "production"
       ? process.env.NEXT_PUBLIC_API_URL_PROD
       : environment === "staging"
       ? process.env.NEXT_PUBLIC_API_URL_STAGING
       : process.env.NEXT_PUBLIC_API_URL_DEV,
+
+  
 };
 
 export default config;
--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import  config  from "../config/config";
+import config from "../config/config.js";
+
 
 const axiosInstance = axios.create({
   baseURL: config.apiUrl,
